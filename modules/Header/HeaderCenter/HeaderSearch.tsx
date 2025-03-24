6078ebd6292fadf2ee48c6b0b7cd047bf7cd4749
Muhammadamin <azizboomnet@gmail.com>
2025-03-24 10:25:41 +0500

    ok

--- modules/Header/HeaderCenter/HeaderSearch.tsx
+++ modules/Header/HeaderCenter/HeaderSearch.tsx
@@ -13,7 +13,7 @@ const HeaderSearch:FC<{isMobile?:boolean}> = ({isMobile}) => {
     <div className={`flex gap-[5px] sm:gap-[10px] ${isMobile ? "hidden-searchbar" :""}`}>
         <Button onBlur={() => setOpenCategory(false)} onClick={() => setOpenCategory(!openCategory)} extraClass='!w-[35%] !h-[40px] sm:!w-[160px] sm:!h-[47px]' icon={<ArrowDownIcon extrClass={`${openCategory && "rotate-[180deg]"}`}/>} iconPosition={"right"}>{t("category")}</Button>
         <div className={`relative ${!isMobile && "w-[65%] sm:w-full"}`}>
-            <Input extraClass={`${!isMobile && "!w-full"} sm:!h-[48px]`} placeholder={t("inputPlaceholder")} type='text' name='search'/>
+            <Input extraClass={`${!isMobile && "!w-full"} outline-0 sm:!h-[48px]`} placeholder={t("inputPlaceholder")} type='text' name='search'/>
             <Button extraClass='!absolute top-0 right-0 !w-[40px] !h-[40px] sm:!h-[48px] sm:!w-[60px] !p-0'> <SearchIcon extraClass='w-[16px] h-[16px] sm:w-[20px] sm:h-[20px]'/> </Button>
         </div>
     </div>
